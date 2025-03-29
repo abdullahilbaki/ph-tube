a659c45a936a32c4138dedc6b54349bd71a2d9d8
abdullahilbaki <abdullahalbaqi00@gmail.com>
2025-03-29 09:32:30 +0600

    added logo and title

---
+++ src/Navbar.tsx
@@ -0,0 +1,11 @@
+import phTubeLogo from "./assets/Logo.png";
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={phTubeLogo} alt="PH Tube" />
+    </div>
+  );
+};
+
+export default Navbar;
