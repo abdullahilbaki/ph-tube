4af1e16f5bf85a60dfd90d6d27e67aa83750d189
abdullahilbaki <abdullahalbaqi00@gmail.com>
2025-03-31 13:13:39 +0600

    added category buttons

--- src/components/CategoryBtns.tsx
+++ src/components/CategoryBtns.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, use } from "react";
 
 interface ButtonProps {
   btnText: string;
@@ -16,15 +16,50 @@ const Button: React.FC<ButtonProps> = ({ btnText }) => {
   );
 };
 
-const CategoryBtns = () => {
+interface Category {
+  category_id: string;
+  category: string;
+}
+
+interface ApiResponse {
+  status: boolean;
+  message: string;
+  categories: Category[];
+}
+
+const ShowCategoryBtns: React.FC<{ buttonPromise: Promise<ApiResponse> }> = ({
+  buttonPromise,
+}) => {
+  const apiResponse: ApiResponse = use(buttonPromise);
+  const categoryBtns: Category[] = apiResponse.categories;
+
+  if (!categoryBtns) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div
       className="container mx-auto px-4 sm:px-0 py-6 flex items-center 
-      justify-center"
+      justify-center gap-3"
     >
       <Button btnText="All" />
+      {categoryBtns.map((category) => (
+        <Button key={category.category_id} btnText={category.category} />
+      ))}
     </div>
   );
 };
 
-export default CategoryBtns;
+const buttonPromise: Promise<ApiResponse> = fetch(
+  "https://openapi.programming-hero.com/api/phero-tube/categories"
+).then((res) => res.json());
+
+const GetCategoryBtns: React.FC = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ShowCategoryBtns buttonPromise={buttonPromise} />
+    </Suspense>
+  );
+};
+
+export default GetCategoryBtns;
